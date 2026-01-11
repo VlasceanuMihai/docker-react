5d284a14bec7eb6eae6e0727f3a77f48cadd1e49
mihaivlasceanu <mihai@luxon-tech.com>
2026-01-11 12:03:16 +0200

    Update master

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learning Git now!
+          Learning Git for spring boot!
         </p>
       </header>
     </div>
