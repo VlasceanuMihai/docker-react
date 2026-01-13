6d200eea8f2c20670fe99b0c389512de4f09d5a0
Mihai Vlasceanu <52928242+VlasceanuMihai@users.noreply.github.com>
2026-01-13 19:53:08 +0200

    Correct capitalization in learning message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learning Git for spring boot!
+          Learning Git for Spring Boot!
         </p>
       </header>
     </div>
